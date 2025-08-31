32538f654e4789ecf8c3bff6a705312ba2570366
Spad0ut <bilan.llrez@gmail.com>
2025-08-31 15:48:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import request, render_template
 def i():
     if request.method == "POST":
         num = float(request.form.get("rates"))
-        return(render_template("index.html", result = 90.2-(50.6*num)))
+        return(render_template("index.html", result = 23.5+(26.3*num)))
     else:
         return render_template("index.html", result ="Waiting……….")
 if __name__=="__main__":
